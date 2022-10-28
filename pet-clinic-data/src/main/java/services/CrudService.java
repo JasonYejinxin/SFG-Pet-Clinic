0acbeb3a683809b41662eb733d3c44146d33e151
JasonYejinxin <jason.jinxinye@gmail.com>
2022-10-27 21:56:32 -0400

    Impl map based services. Closes #6

--- pet-clinic-data/src/main/java/services/CrudService.java
+++ pet-clinic-data/src/main/java/services/CrudService.java
@@ -6,11 +6,11 @@ public interface CrudService<T,ID>{
 
     Set<T> findAll();
 
-    T findById();
+    T findById(ID id);
 
     T save(T object);
 
     void delete(T object);
 
-    void deleteBId(ID id);
+    void deleteById(ID id);
 }
