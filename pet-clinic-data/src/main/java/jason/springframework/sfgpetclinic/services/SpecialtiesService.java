5eb992467fd1ec390849a90d61beff31b002d00b
JasonYejinxin <jason.jinxinye@gmail.com>
2022-11-23 22:53:49 -0500

    add specialties map impl #31

---
+++ pet-clinic-data/src/main/java/jason/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package jason.springframework.sfgpetclinic.services;
+
+import jason.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty,Long>{
+}
